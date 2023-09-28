7302f0e3fbaa9fb2a577add8c9517a2821b320c4
Mohit <Ultra>
2023-09-28 16:23:20 +0530

    recursion

--- Array_ReverseArray.py
+++ Array_ReverseArray.py
@@ -1,16 +1,13 @@
 # Python program to reverse an array
-  
-# Function to reverse A[] from start to end
 def reverseList(A, start, end):
-    while start < end:
-        A[start], A[end] = A[end], A[start]
-        start += 1
-        end -= 1
-  
+    if start >= end:
+        return
+    A[start], A[end] = A[end], A[start]
+    reverseList(A, start+1, end-1)
+
 # Driver function to test above function
 A = [1, 2, 3, 4, 5, 6]
-print(f"Original list is",A)
-reverseList(A, 0, 5)
-print(f"Reversed list is",A)
-
+print("Original list is",A)
+reverseList(A, 0, len(A)-1)
+print("Reversed list is",A)
 #ends here
